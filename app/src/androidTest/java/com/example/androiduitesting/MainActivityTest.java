707db061f3cef157c066eebe089ac763f88fde0c
nzapisocki <nzapisoc@ualberta.ca>
2026-02-27 13:24:26 -0700

    Update MainActivityTest.java

--- app/src/androidTest/java/com/example/androiduitesting/MainActivityTest.java
+++ app/src/androidTest/java/com/example/androiduitesting/MainActivityTest.java
@@ -3,16 +3,16 @@ package com.example.androiduitesting;
 import static androidx.test.espresso.Espresso.onData;
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
-
 import static androidx.test.espresso.assertion.ViewAssertions.doesNotExist;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
-
+import static org.hamcrest.CoreMatchers.anything;
 import static org.hamcrest.CoreMatchers.instanceOf;
 import static org.hamcrest.CoreMatchers.is;
 
+import androidx.test.core.app.ActivityScenario;
 import androidx.test.espresso.action.ViewActions;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
@@ -29,27 +29,27 @@ public class MainActivityTest {
     public ActivityScenarioRule<MainActivity> scenario = new ActivityScenarioRule<MainActivity>(MainActivity.class);
 
     @Test
-    public void testAddCity(){
-    // Click on Add City button
+    public void testAddCity() {
+// Click on Add City button
         onView(withId(R.id.button_add)).perform(click());
-    // Type "Edmonton" in the editText
+// Type "Edmonton" in the editText
         onView(withId(R.id.editText_name)).perform(ViewActions.typeText("Edmonton"));
-    // Click on Confirm
+// Click on Confirm
         onView(withId(R.id.button_confirm)).perform(click());
-    // Check if text "Edmonton" is matched with any of the text displayed on the screen
+// Check if text "Edmonton" is matched with any of the text displayed on the screen
         onView(withText("Edmonton")).check(matches(isDisplayed()));
     }
 
     @Test
-    public void testClearCity(){
+    public void testClearCity() {
 // Add first city to the list
         onView(withId(R.id.button_add)).perform(click());
         onView(withId(R.id.editText_name)).perform(ViewActions.typeText("Edmonton"));
-                onView(withId(R.id.button_confirm)).perform(click());
+        onView(withId(R.id.button_confirm)).perform(click());
 //Add another city to the list
         onView(withId(R.id.button_add)).perform(click());
         onView(withId(R.id.editText_name)).perform(ViewActions.typeText("Vancouver"));
-                onView(withId(R.id.button_confirm)).perform(click());
+        onView(withId(R.id.button_confirm)).perform(click());
 //Clear the list
         onView(withId(R.id.button_clear)).perform(click());
         onView(withText("Edmonton")).check(doesNotExist());
@@ -57,16 +57,15 @@ public class MainActivityTest {
     }
 
     @Test
-    public void testListView(){
+    public void testListView() {
 // Add a city
         onView(withId(R.id.button_add)).perform(click());
         onView(withId(R.id.editText_name)).perform(ViewActions.typeText("Edmonton"));
-                onView(withId(R.id.button_confirm)).perform(click());
-// Check if in the Adapter view (given id of that adapter view),there is a data
+        onView(withId(R.id.button_confirm)).perform(click());
+// Check if in the Adapter view (given id of that adapter view), there is a data
 // (which is an instance of String) located at position zero.
 // If this data matches the text we provided then Voila! Our test should pass
 // You can also use anything() in place of is(instanceOf(String.class))
-        onData(is(instanceOf(String.class))).inAdapterView(withId(R.id.city_list
-        )).atPosition(0).check(matches((withText("Edmonton"))));
+        onData(is(instanceOf(String.class))).inAdapterView(withId(R.id.city_list)).atPosition(0).check(matches((withText("Edmonton"))));
     }
 }
